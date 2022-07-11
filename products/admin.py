e1ec57220a0b47945687e019f55aaf554027f48f
312Avenue <djumagaziev026@mail.ru>
2022-07-11 18:10:56 +0600

    v2

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from products.models import Brand, Comment, Product, SneakersType
+
+admin.site.register(Product)
+admin.site.register(SneakersType)
+admin.site.register(Brand)
+admin.site.register(Comment)
