cbe9cd978a01aa42f5d6f91dd6a3cabb95ccaf3e
312Avenue <djumagaziev026@mail.ru>
2022-07-11 21:17:03 +0600

    v1

--- products/models.py
+++ products/models.py
@@ -62,6 +62,7 @@ class Product(models.Model):
     size = models.CharField(max_length=20, choices=SIZE_CHOICE)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField()
 
     def __str__(self):
         return self.title
