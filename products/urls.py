7a6c4f1b5ec66507f4990a8f7b8f290a848a66ae
312Avenue <djumagaziev026@mail.ru>
2022-07-13 16:13:43 +0600

    orders urls

--- products/urls.py
+++ products/urls.py
@@ -5,7 +5,7 @@ from rest_framework.routers import DefaultRouter
 
 
 router = DefaultRouter()
-router.register('', ProductViewSet)
+router.register('products', ProductViewSet)
 
 urlpatterns = []
 urlpatterns += router.urls # urlpatterns.extend(router.urls)
