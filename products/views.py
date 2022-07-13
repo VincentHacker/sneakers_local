7766e1c245888bfbd7f783810512c8ca75440267
312Avenue <djumagaziev026@mail.ru>
2022-07-13 14:43:20 +0600

    add permishions

--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
+from rest_framework import permissions
 
 from products.filters import ProductPriceFilter
 
@@ -12,7 +13,7 @@ from .serializers import ProductSerializer, ProductListSerializer
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filter_backends = [SearchFilter, DjangoFilterBackend]
     search_fields = ['title', 'description']
     filterset_class = ProductPriceFilter
 
@@ -20,3 +21,10 @@ class ProductViewSet(ModelViewSet):
         if self.action == 'list':
             return ProductListSerializer
         return super().get_serializer_class()
+
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            self.permission_classes = [permissions.AllowAny]
+        elif self.action in ['destroy', 'update', 'partial_update', 'create']:
+            self.permission_classes = [permissions.IsAdminUser]
+        return super().get_permissions()
