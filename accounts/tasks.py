d534d1326bb2927fe40f02eb5441e4a5b5b27cdd
312Avenue <djumagaziev026@mail.ru>
2022-07-19 16:55:04 +0600

    without celery

--- accounts/tasks.py
+++ accounts/tasks.py
@@ -1,14 +1,14 @@
-from celery import shared_task
-from django.conf import settings
-from django.core.mail import send_mail
+# from celery import shared_task
+# from django.conf import settings
+# from django.core.mail import send_mail
 
-@shared_task
-def send_activation_code(email, activation_code):
-    activation_link = f'https://dry-sands-45075.herokuapp.com/account/activation/{activation_code}'
-    send_mail(
-        'Account activation', 
-        message=activation_link, 
-        from_email=settings.EMAIL_HOST_USER, 
-        recipient_list=[email], 
-        fail_silently=False
-    )
+# @shared_task
+# def send_activation_code(email, activation_code):
+#     activation_link = f'https://dry-sands-45075.herokuapp.com/account/activation/{activation_code}'
+#     send_mail(
+#         'Account activation', 
+#         message=activation_link, 
+#         from_email=settings.EMAIL_HOST_USER, 
+#         recipient_list=[email], 
+#         fail_silently=False
+#     )
--- sneakers/settings.py
+++ sneakers/settings.py
@@ -54,8 +54,8 @@ INSTALLED_APPS = [
     'orders',
     'products',
 
-    'django_celery_beat',
-    'django_celery_results',
+    # 'django_celery_beat',
+    # 'django_celery_results',
 ]
 
 MIDDLEWARE = [    
@@ -216,12 +216,12 @@ SWAGGER_SETTINGS = {
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-CELERY_BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+# CELERY_BROKER_URL = 'redis://localhost:6379'
+# CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 
-CELERY_ACCEPT_CONTENT = ['json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TIMEZONE = 'UTC'
-REDIS_HOST = 'localhost'
-REDIS_PORT = '6379'
+# CELERY_ACCEPT_CONTENT = ['json']
+# CELERY_TASK_SERIALIZER = 'json'
+# CELERY_RESULT_SERIALIZER = 'json'
+# CELERY_TIMEZONE = 'UTC'
+# REDIS_HOST = 'localhost'
+# REDIS_PORT = '6379'
